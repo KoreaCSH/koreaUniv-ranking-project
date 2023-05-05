cf614b94ece33bfb012ab38f32c630f429d251cd
csh <tmdgjs7495@naver.com>
2023-05-05 22:54:51 +0900

    [style] : ErrorResult 변수명 변경

--- src/main/java/koreaUniv/koreaUnivRankSys/global/exception/ErrorResult.java
+++ src/main/java/koreaUniv/koreaUnivRankSys/global/exception/ErrorResult.java
@@ -1,13 +1,13 @@
 package koreaUniv.koreaUnivRankSys.global.exception;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.Getter;
 
-@Data
+@Getter
 @AllArgsConstructor
 public class ErrorResult {
 
-    private String code;
+    private String status;
     private String message;
 
 }
