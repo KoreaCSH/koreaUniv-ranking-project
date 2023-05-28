94bc6223a5e5d4cafaffcba47477dfb196a48a7c
eujin-shin <eujinshin0124@gmail.com>
2023-05-28 17:52:16 +0900

    [2] 오류 수정 및 주석 추가, D.S 수정 필요

--- src/main/java/koreaUniv/koreaUnivRankSys/domain/member/repository/DepartmentRepository.java
+++ src/main/java/koreaUniv/koreaUnivRankSys/domain/member/repository/DepartmentRepository.java
@@ -2,10 +2,12 @@ package koreaUniv.koreaUnivRankSys.domain.member.repository;
 
 import koreaUniv.koreaUnivRankSys.domain.member.domain.College;
 import koreaUniv.koreaUnivRankSys.domain.member.domain.Department;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository // department의 경우에만 오류가 발생해서 annotation 사용해 둠
 public interface DepartmentRepository {
     Optional<Department> findByName(String name);
 
