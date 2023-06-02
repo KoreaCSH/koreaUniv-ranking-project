5d5a0724f0a947b44614280b9dd8667559d477d0
eujin-shin <eujinshin0124@gmail.com>
2023-06-03 00:10:28 +0900

    단과대학, 학과 1차 수정

--- src/main/java/koreaUniv/koreaUnivRankSys/domain/member/service/DepartmentService.java
+++ src/main/java/koreaUniv/koreaUnivRankSys/domain/member/service/DepartmentService.java
@@ -1,25 +1,17 @@
 package koreaUniv.koreaUnivRankSys.domain.member.service;
 
-<<<<<<< HEAD
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Service;
+
 import koreaUniv.koreaUnivRankSys.domain.member.domain.College;
 import koreaUniv.koreaUnivRankSys.domain.member.domain.Department;
 import koreaUniv.koreaUnivRankSys.domain.member.repository.DepartmentRepository;
 import koreaUniv.koreaUnivRankSys.global.exception.CustomException;
 import koreaUniv.koreaUnivRankSys.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-=======
-import java.util.List;
->>>>>>> 55f884dfcc6ff5ef9cd31a1f5f101dcb68e7aa87
-
-import javax.transaction.Transactional;
-import java.util.List;
-
-import org.springframework.stereotype.Service;
-
-import koreaUniv.koreaUnivRankSys.domain.member.domain.College;
-import koreaUniv.koreaUnivRankSys.domain.member.domain.Department;
-import koreaUniv.koreaUnivRankSys.domain.member.repository.DepartmentRepository;
 
 @Service
 @Transactional
