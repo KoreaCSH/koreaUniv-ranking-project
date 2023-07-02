a0f75d2bd1641d653c732cd33b74a6da067ee4e4
csh <tmdgjs7495@naver.com>
2023-07-02 20:48:54 +0900

    [feat] : MonthlyRankingDtoRowMapper 구현

---
+++ src/main/java/koreaUniv/koreaUnivRankSys/domain/building/dto/mapper/MonthlyRankingDtoRowMapper.java
@@ -0,0 +1,20 @@
+package koreaUniv.koreaUnivRankSys.domain.building.dto.mapper;
+
+import koreaUniv.koreaUnivRankSys.domain.building.dto.RankingDto;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class MonthlyRankingDtoRowMapper implements RowMapper<RankingDto> {
+
+    @Override
+    public RankingDto mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return RankingDto.builder()
+                .nickName(rs.getString("nick_name"))
+                .imageUrl(rs.getString("path"))
+                .studyTime(rs.getLong("monthly_study_time"))
+                .ranking(rs.getLong("ranking"))
+                .build();
+    }
+}
