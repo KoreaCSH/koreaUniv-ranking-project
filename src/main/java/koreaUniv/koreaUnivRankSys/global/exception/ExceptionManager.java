57f8abc04a1b00fafaed0f32d4106493ab851318
csh <tmdgjs7495@naver.com>
2023-08-23 15:01:45 +0900

    [feat] : department 조회 API PathVariable Validation 추가

--- src/main/java/koreaUniv/koreaUnivRankSys/global/exception/ExceptionManager.java
+++ src/main/java/koreaUniv/koreaUnivRankSys/global/exception/ExceptionManager.java
@@ -7,15 +7,27 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import javax.validation.ConstraintViolationException;
+
 @RestControllerAdvice
 public class ExceptionManager {
 
+    // request, Dto 관련 Exception
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> methodArgumentNotValidExceptionHandler(MethodArgumentNotValidException e) {
         String message = e.getBindingResult()
                 .getAllErrors()
                 .get(0)
                 .getDefaultMessage();
+
+        return ResponseEntity.badRequest()
+                             .body(message);
+    }
+
+    // RequestParam, PathVariable 관련 Exception
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<?> constraintViolationException(ConstraintViolationException e) {
+        String message = e.getMessage();
 
         return ResponseEntity.badRequest()
                              .body(message);
--- src/main/java/koreaUniv/koreaUnivRankSys/web/member/DepartmentController.java
+++ src/main/java/koreaUniv/koreaUnivRankSys/web/member/DepartmentController.java
@@ -1,17 +1,18 @@
 package koreaUniv.koreaUnivRankSys.web.member;
 
 import koreaUniv.koreaUnivRankSys.domain.member.service.DepartmentService;
+import koreaUniv.koreaUnivRankSys.global.exception.CustomException;
+import koreaUniv.koreaUnivRankSys.global.exception.ErrorCode;
 import koreaUniv.koreaUnivRankSys.web.member.dto.DepartmentDto;
 import koreaUniv.koreaUnivRankSys.web.member.dto.DepartmentResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RestController
 @RequestMapping("/api/department")
 @RequiredArgsConstructor
@@ -19,8 +20,13 @@ public class DepartmentController {
 
     private final DepartmentService departmentService;
 
-    @GetMapping("{collegeName}")
+    @GetMapping("/departments/{collegeName}")
     public ResponseEntity<DepartmentResponse> getDepartmentList(@PathVariable String collegeName) {
+
+        if(collegeName.isBlank()) {
+            throw new CustomException(ErrorCode.COLLEGE_NOTFOUND);
+        }
+
         List<DepartmentDto> departmentList = departmentService.findByCollegeName(collegeName);
 
         return ResponseEntity.ok().body(DepartmentResponse.of(departmentList));
